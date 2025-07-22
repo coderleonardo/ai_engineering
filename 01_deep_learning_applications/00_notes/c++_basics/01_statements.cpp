7496c18e2dfb0df0ba8548f2a701aebd16166c27
coderleo <coderleonardo@gmail.com>
2025-07-22 08:41:53 -0300

    cleaning the folders

--- 01_deep_learning_applications/00_notes/c++_basics/01_statements.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream> 
-using namespace std;
-
-int main() {
-    // Output a message to the console
-    cout << "Hello, World!" << endl; // Print "Hello, World!" followed by a new line
-    return 0; // Return 0 to indicate successful execution
-} // End of main function 
